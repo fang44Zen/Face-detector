e647d65868c80636fcc257131e335aeaeb7438f5
fang44Zen <lyons.fr55@gmail.com>
2022-11-16 22:22:27 +0100

    adding states + styles

--- src/App.js
+++ src/App.js
@@ -5,14 +5,30 @@ import ParticlesBg from "particles-bg";
 
 import Navigation from "./Components/Navigation/navigation.component";
 import Rank from "./rank/rank.component";
+import { useState } from "react";
 
 function App() {
+  const [mainInput, setMainInput] = useState("");
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+    setMainInput(event.target.value);
+  };
+
+  const onSubmit = () => {
+    console.log("click");
+  };
+
   return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        value={mainInput}
+        onSubmit={onSubmit}
+      />
       <ParticlesBg type="cobweb" bg={true} num={200} />
     </div>
   );
--- src/Components/imagelinkform/imagelinkform.componenent.jsx
+++ src/Components/imagelinkform/imagelinkform.componenent.jsx
@@ -1,13 +1,20 @@
 import "./imagelinkform.scss";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className="fs-3">{"This brain will detect faces in your pictures."}</p>
       <div className="d-flex justify-content-center">
         <div className="form pa4 br3 shadow-5 d-flex align-items">
-          <input className="fs-4 p-2 w-70 center" type="text" />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          <input
+            className="fs-4 p-2 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onSubmit}
+          >
             Detect
           </button>
         </div>

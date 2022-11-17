6bb10a514219387c9802332388ebaa5e83acd000
fang44Zen <lyons.fr55@gmail.com>
2022-11-17 17:31:22 +0100

    API outdated hmm..

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import ImageLinkForm from "./Components/imagelinkform/imagelinkform.componenent";
 import Logo from "./Components/Logo/logo.component";
 import ParticlesBg from "particles-bg";
-
+import Clarifai from "clarifai";
 import Navigation from "./Components/Navigation/navigation.component";
 import Rank from "./rank/rank.component";
 import { useState } from "react";
+
+const app = new Clarifai.App({
+  apiKey: "6950ded4e8154ad892ab26f4a7989bf1",
+});
 
 function App() {
   const [mainInput, setMainInput] = useState("");

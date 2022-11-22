23b1966a52a71ebfcf77940e5dc677e88af21e99
fang44Zen <lyons.fr55@gmail.com>
2022-11-22 14:04:02 +0100

    issues avec API, dernier back-up avant audit fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
+import Clarifai from "clarifai";
 import ImageLinkForm from "./Components/imagelinkform/imagelinkform.componenent";
 import Logo from "./Components/Logo/logo.component";
 import ParticlesBg from "particles-bg";
-import Clarifai from "clarifai";
 import Navigation from "./Components/Navigation/navigation.component";
 import Rank from "./rank/rank.component";
 import { useState } from "react";
-
-const app = new Clarifai.App({
-  apiKey: "6950ded4e8154ad892ab26f4a7989bf1",
-});
 
 function App() {
   const [mainInput, setMainInput] = useState("");
@@ -20,7 +16,22 @@ function App() {
   };
 
   const onSubmit = () => {
+    let app = new Clarifai.App({
+      apiKey: '6950ded4e8154ad892ab26f4a7989bf1"',
+    });
     console.log("click");
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        // THE JPG
+        "https://i.insider.com/5d321d4ea209d3146d650b4a?width=1100&format=jpeg&auto=webp"
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
